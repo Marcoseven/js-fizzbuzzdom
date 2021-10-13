0bbb6f82fea149294d0ee04346be6f6b445a9b98
Marcoseven <marco.caliciotti3@gmail.com>
2021-10-13 16:13:06 +0200

    Aggiunte modifiche
    Modificato i commenti e inserito un if all'interno del ciclo for

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,13 +3,23 @@ Scrivi un programma che esegua un ciclo da 1 a 100 e a ogni iterazione appenda u
 per i multipli di 3, per i multipli di 5 e per i valori che sono sia multipli di 3 che di 5. 
 */
 
-// variabile
+// variabili
 let list = document.querySelector(".list");
+const multiple_Three = 3;
+const multiple_Five = 5;
+const multiple_Fifteen = 15;
 
-// ciclo for
+// ciclo for con if
 for (let i = 1; i <= 100; i++) {
 	console.log(i);
-	const element = `<li class="box"> ${i}</li>`;
-	list.innerHTML += element;
+	if (i % multiple_Fifteen === 0) {
+		list.insertAdjacentHTML("beforeend", `<div class="box_15">fizzbuzz</div>`);
+	} else if (i % multiple_Five === 0) {
+		list.insertAdjacentHTML("beforeend", `<div class="box_5">buzz</div>`);
+	} else if (i % multiple_Three === 0) {
+		list.insertAdjacentHTML("beforeend", `<div class="box_3">fizz</div>`);
+	} else {
+		list.insertAdjacentHTML("beforeend", `<div class="box">${i}</div>`);
+	}
 }
 console.log("Fine");
